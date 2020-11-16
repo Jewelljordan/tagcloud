c52cbd9b27baccf9756a00fc4d8173c0d5710be4
Ramya Iyer <ramyaiyer1000@gmail.com>
2020-11-16 10:39:51 -0600

    Add files via upload

--- TagCloud.java
+++ TagCloud.java
@@ -3,34 +3,52 @@ import java.net.URLConnection;
 import java.io.IOException;
 import java.util.Scanner;
 import java.net.MalformedURLException;
-public class tagCloud
-{
-	static String s;
-public static void run()throws Exception
- {
-try {
- URL myURL = new URL("https://news.ycombinator.com/"); //https://www.unm.edu/~gmartin/535/Sticks.htm
- URLConnection site = myURL.openConnection();
- site.connect();
- Scanner file = new Scanner( site.getInputStream() );
- s= file.useDelimiter( "\\Z" ).next() ;
-}
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
-catch (MalformedURLException e) {
- // new URL() failed
- // ...
-}
-catch (IOException e) {
- // openConnection() failed
- // ...
-}
- }
- public static String getUrl()
- {
-	 return s;
- }
- public static void main(String[] args) throws Exception
- {
-	 tagCloud.run();
- }
+public class TagCloud extends JFrame {
+	static String s;
+	static websiteData sitewebsiteData;
+	static graphicCloud graphic;
+	static final int WIDTH = 1200, HEIGHT = 800;
+	
+	public TagCloud(){
+		super("TagCloud");
+		
+		setSize(WIDTH,HEIGHT);
+		getContentPane().add(graphic);
+		setVisible(true);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//used for graphics
+		BufferedImage image = new BufferedImage ( graphic.getWidth (), graphic.getHeight (), BufferedImage.TYPE_INT_ARGB );
+        Graphics2D graphics2D = image.createGraphics ();
+        graphic.paintAll ( graphics2D );
+        graphics2D.graphicose ();
+
+        try {
+            ImageIO.write ( image, "png", new File("image.png"));
+        }
+        catch ( IOException e ) {
+            e.printStackTrace ();
+        }
+
+		
+		System.out.println("size: " + getContentPane().getSize().toString());
+		System.out.println("height: " + getContentPane().getHeight());
+		
+	}
+	
+	public static void main(String[] args) throws Exception {
+		String url = "https://www.genshin.cc/";
+		GetURL scr = new GetURL(url); //we need a file/class that gets the data from the website
+		String s = scr.getWebsitewebsiteData();
+		System.out.println(s);
+		sitewebsiteData = new websiteData(s);	//the data from the website
+		graphic = new Display(sitewebsiteData.getMax(),sitewebsiteData.getCloud()); //using the graphics
+		
+		new TagCloud();
+	}
 }
