35e69fcd633338e7342d6607db2b1077385dae03
Brooklyndanner <73569025+Brooklyndanner@users.noreply.github.com>
2020-11-17 18:12:38 -0600

    Update mapCloud.java

--- mapCloud.java
+++ mapCloud.java
@@ -28,10 +28,18 @@ static String current="";
 			String[] title=current.split(" ");
 			for(String s:title)   //adds all of the words in title to cloud map with their correct numerical value
 			{
-				if(cloud.containsKey(s))
-					cloud.put(s,cloud.get(s)+10);
-				if(!cloud.containsKey(s))
-					cloud.put(s, 10);	
+				if(s.length()>1 && (s.substring(s.length()-1).equals(".") || s.substring(s.length()-1).equals(","))) {
+				if(cloud.containsKey(s.substring(0,s.length()-1)))
+					cloud.put(s,cloud.get(s.substring(0,s.length()-1))+10);
+				if(!cloud.containsKey(s.substring(0,s.length()-1)))
+					cloud.put(s.substring(0,s.length()-1), 10);	}
+				else
+				{
+					if(cloud.containsKey(s))
+						cloud.put(s,cloud.get(s)+10);
+					if(!cloud.containsKey(s))
+						cloud.put(s, 10);
+				}
 			}
 			url=url.replaceFirst("<title>.*</title>"," ");  //this removes the title from the html (it works)
 			//System.out.println(url);
@@ -48,10 +56,18 @@ static String current="";
 			String[] h1=current.split(" ");
 			for(String s:h1)   //adds all of the words in title to cloud map with their correct numerical value
 			{
-				if(cloud.containsKey(s))
-					cloud.put(s,cloud.get(s)+10);
-				if(!cloud.containsKey(s))
-					cloud.put(s, 10);	
+				if(s.length()>1 && (s.substring(s.length()-1).equals(".") || s.substring(s.length()-1).equals(","))) {
+					if(cloud.containsKey(s.substring(0,s.length()-1)))
+						cloud.put(s,cloud.get(s.substring(0,s.length()-1))+10);
+					if(!cloud.containsKey(s.substring(0,s.length()-1)))
+						cloud.put(s.substring(0,s.length()-1), 10);	}
+					else
+					{
+						if(cloud.containsKey(s))
+							cloud.put(s,cloud.get(s)+10);
+						if(!cloud.containsKey(s))
+							cloud.put(s, 10);
+					}	
 			}
 			url=url.replaceFirst("<h1>.*</h1>"," ");
 		}
@@ -63,10 +79,18 @@ static String current="";
 			String[] h2=current.split(" ");
 			for(String s:h2)   //adds all of the words in title to cloud map with their correct numerical value
 			{
-				if(cloud.containsKey(s))
-					cloud.put(s,cloud.get(s)+8);
-				if(!cloud.containsKey(s))
-					cloud.put(s, 8);	
+				if(s.length()>1 && (s.substring(s.length()-1).equals(".") || s.substring(s.length()-1).equals(","))) {
+					if(cloud.containsKey(s.substring(0,s.length()-1)))
+						cloud.put(s,cloud.get(s.substring(0,s.length()-1))+8);
+					if(!cloud.containsKey(s.substring(0,s.length()-1)))
+						cloud.put(s.substring(0,s.length()-1), 8);	}
+					else
+					{
+						if(cloud.containsKey(s))
+							cloud.put(s,cloud.get(s)+8);
+						if(!cloud.containsKey(s))
+							cloud.put(s, 8);
+					}	
 			}
 			url=url.replaceFirst("<h2>.*</h2>"," ");
 		}
@@ -78,10 +102,18 @@ static String current="";
 			String[] h3=current.split(" ");
 			for(String s:h3)   //adds all of the words in title to cloud map with their correct numerical value
 			{
-				if(cloud.containsKey(s))
-					cloud.put(s,cloud.get(s)+6);
-				if(!cloud.containsKey(s))
-					cloud.put(s, 6);	
+				if(s.length()>1 && (s.substring(s.length()-1).equals(".") || s.substring(s.length()-1).equals(","))) {
+					if(cloud.containsKey(s.substring(0,s.length()-1)))
+						cloud.put(s,cloud.get(s.substring(0,s.length()-1))+6);
+					if(!cloud.containsKey(s.substring(0,s.length()-1)))
+						cloud.put(s.substring(0,s.length()-1), 6);	}
+					else
+					{
+						if(cloud.containsKey(s))
+							cloud.put(s,cloud.get(s)+6);
+						if(!cloud.containsKey(s))
+							cloud.put(s, 6);
+					}	
 			}
 			url=url.replaceFirst("<h3>.*</h3>"," ");
 		}
@@ -93,10 +125,18 @@ static String current="";
 			String[] h4=current.split(" ");
 			for(String s:h4)   //adds all of the words in title to cloud map with their correct numerical value
 			{
-				if(cloud.containsKey(s))
-					cloud.put(s,cloud.get(s)+4);
-				if(!cloud.containsKey(s))
-					cloud.put(s, 4);	
+				if(s.length()>1 && (s.substring(s.length()-1).equals(".") || s.substring(s.length()-1).equals(","))) {
+					if(cloud.containsKey(s.substring(0,s.length()-1)))
+						cloud.put(s,cloud.get(s.substring(0,s.length()-1))+4);
+					if(!cloud.containsKey(s.substring(0,s.length()-1)))
+						cloud.put(s.substring(0,s.length()-1), 4);	}
+					else
+					{
+						if(cloud.containsKey(s))
+							cloud.put(s,cloud.get(s)+4);
+						if(!cloud.containsKey(s))
+							cloud.put(s, 4);
+					}	
 			}
 			url=url.replaceFirst("<h4>.*</h4>"," ");
 		}
@@ -108,10 +148,18 @@ static String current="";
 			String[] h5=current.split(" ");
 			for(String s:h5)   //adds all of the words in title to cloud map with their correct numerical value
 			{
-				if(cloud.containsKey(s))
-					cloud.put(s,cloud.get(s)+2);
-				if(!cloud.containsKey(s))
-					cloud.put(s, 2);	
+				if(s.length()>1 && (s.substring(s.length()-1).equals(".") || s.substring(s.length()-1).equals(","))) {
+					if(cloud.containsKey(s.substring(0,s.length()-1)))
+						cloud.put(s,cloud.get(s.substring(0,s.length()-1))+2);
+					if(!cloud.containsKey(s.substring(0,s.length()-1)))
+						cloud.put(s.substring(0,s.length()-1), 2);	}
+					else
+					{
+						if(cloud.containsKey(s))
+							cloud.put(s,cloud.get(s)+2);
+						if(!cloud.containsKey(s))
+							cloud.put(s, 2);
+					}	
 			}
 			url=url.replaceFirst("<h5>.*</h5>"," ");
 		}
@@ -123,10 +171,18 @@ static String current="";
 			String[] h6=current.split(" ");
 			for(String s:h6)   //adds all of the words in title to cloud map with their correct numerical value
 			{
-				if(cloud.containsKey(s))
-					cloud.put(s,cloud.get(s)+1);
-				if(!cloud.containsKey(s))
-					cloud.put(s, 1);	
+				if(s.length()>1 && (s.substring(s.length()-1).equals(".") || s.substring(s.length()-1).equals(","))) {
+					if(cloud.containsKey(s.substring(0,s.length()-1)))
+						cloud.put(s,cloud.get(s.substring(0,s.length()-1))+1);
+					if(!cloud.containsKey(s.substring(0,s.length()-1)))
+						cloud.put(s.substring(0,s.length()-1), 1);	}
+					else
+					{
+						if(cloud.containsKey(s))
+							cloud.put(s,cloud.get(s)+1);
+						if(!cloud.containsKey(s))
+							cloud.put(s, 1);
+					}	
 			}
 			url=url.replaceFirst("<h6>.*</h6>"," ");
 		}
@@ -152,32 +208,88 @@ static String current="";
 			//System.out.println(lastcarrot);
 			//System.out.println("A");
 			String[] a=lastcarrot.split(" ");
-			for(String place:a)   //adds all of the words in title to cloud map with their correct numerical value
+			for(String s:a)   //adds all of the words in title to cloud map with their correct numerical value
 			{
-				if(cloud.containsKey(place))
-					cloud.put(place,cloud.get(place)+5);     
-				if(!cloud.containsKey(place))
-					cloud.put(place, 5);	
+				if(s.length()>1 && (s.substring(s.length()-1).equals(".") || s.substring(s.length()-1).equals(","))) {
+					if(cloud.containsKey(s.substring(0,s.length()-1)))
+						cloud.put(s,cloud.get(s.substring(0,s.length()-1))+5);
+					if(!cloud.containsKey(s.substring(0,s.length()-1)))
+						cloud.put(s.substring(0,s.length()-1), 5);	}
+					else
+					{
+						if(cloud.containsKey(s))
+							cloud.put(s,cloud.get(s)+5);
+						if(!cloud.containsKey(s))
+							cloud.put(s, 5);
+					}	
 				//System.out.println(place);
 			}
 			url=url.substring(0,first) + url.substring(last+4);         // 	<a  </a>																						 //[\\<]a\\s.*[\\<][\\/]a[\\>]
 			
 		}
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		/*while(url.contains("<ul>") && url.contains("</ul>"))
+		while(url.contains("<ul>") && url.contains("</ul>"))
 		{
 			first=url.indexOf("<ul>"); 
 			last=url.indexOf("</ul>"); 
 			current=url.substring(first,last);
-			String[] list=current.split("<ul>");
-			//make sure the remove white space 
-		}*/
+			String[] list=current.split("</li>");
+			//make sure to remove white space 
+			ArrayList<String> bulletlist= new ArrayList<String>();
+			for(String str:list)
+			{
+				bulletlist.add(str);
+			}
+			for(int x=0; x<bulletlist.size(); x++)
+			{
+				bulletlist.get(x).replaceAll("<li>", "");
+				bulletlist.get(x).replaceAll("\\s", "");
+					
+			}
+		}
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		while(url.contains("<p class") && url.contains("</p>"))
 		{
 			first=url.indexOf("<p class");
 			last=url.indexOf("</p>");
 			current=url.substring(first+7,last);
+			
+			
+			while(current.contains("<a href") && current.contains("</a>"))
+			{
+				int firstone=url.indexOf("<a "); 
+				int lastone=url.indexOf("</a>");
+				String currentone=url.substring(firstone+2,lastone);
+				String lastcarrot= current.substring(currentone.lastIndexOf(">"));
+				if(lastcarrot.length()>1) //cut out the carrot key
+				{
+					lastcarrot=lastcarrot.substring(1);
+				}
+				else
+				{
+					lastcarrot="";
+				}
+				String[] a=lastcarrot.split(" ");
+				for(String place:a)   //adds all of the words in title to cloud map with their correct numerical value
+				{
+					if(s.length()>1 && (s.substring(s.length()-1).equals(".") || s.substring(s.length()-1).equals(","))) {
+						if(cloud.containsKey(s.substring(0,s.length()-1)))
+							cloud.put(s,cloud.get(s.substring(0,s.length()-1))+5);
+						if(!cloud.containsKey(s.substring(0,s.length()-1)))
+							cloud.put(s.substring(0,s.length()-1), 5);	}
+						else
+						{
+							if(cloud.containsKey(s))
+								cloud.put(s,cloud.get(s)+5);
+							if(!cloud.containsKey(s))
+								cloud.put(s, 5);
+						}	
+					//System.out.println(place);
+				}
+				current=current.substring(0,first) + current.substring(last+4);
+			}
+			
+			
 			String lastcarrot=current.substring(current.lastIndexOf(">"));
 			if(lastcarrot.length()>1)
 			{
@@ -188,12 +300,20 @@ static String current="";
 				lastcarrot="";
 			}
 			String[] a=lastcarrot.split(" ");
-			for(String place:a)   //adds all of the words in title to cloud map with their correct numerical value
+			for(String s:a)   //adds all of the words in title to cloud map with their correct numerical value
 			{
-				if(cloud.containsKey(place))
-					cloud.put(place,cloud.get(place)+5);     
-				if(!cloud.containsKey(place))
-					cloud.put(place, 5);	
+				if(s.length()>1 && (s.substring(s.length()-1).equals(".") || s.substring(s.length()-1).equals(","))) {
+					if(cloud.containsKey(s.substring(0,s.length()-1)))
+						cloud.put(s,cloud.get(s.substring(0,s.length()-1))+1);
+					if(!cloud.containsKey(s.substring(0,s.length()-1)))
+						cloud.put(s.substring(0,s.length()-1), 1);	}
+					else
+					{
+						if(cloud.containsKey(s))
+							cloud.put(s,cloud.get(s)+1);
+						if(!cloud.containsKey(s))
+							cloud.put(s, 1);
+					}	
 				//System.out.println(place);
 			}
 			url=url.substring(0,first) + url.substring(last+4);
@@ -201,10 +321,11 @@ static String current="";
 		
 		if(cloud.containsKey(""))
 			cloud.remove("");
+		if(cloud.containsKey(","))
+			cloud.remove(",");
 		System.out.println(url);
 		System.out.println();
 		System.out.println(cloud);
-		System.out.println("a");
 	
 	
 
